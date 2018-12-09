199c9f9edf5321666697c604dda901b6d8d94c1e
heven1d <44444524+heven1d@users.noreply.github.com>
2018-12-09 16:06:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setActivity("You.",{type: 'LISTENING'});
+client.user.setActivity("You.",{type: 'WATCHING'});
   console.log('')
   console.log('')
   console.log('╔[═══════════════════════════════]╗')
