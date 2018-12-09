e90cd60d8c07a1131e2957b4e2b40b44c4968e03
heven1d <44444524+heven1d@users.noreply.github.com>
2018-12-09 15:50:32 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,47 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "w";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
           client.user.setActivity("You.",{type: 'WATCHING'});
-
-
-//bc
-
-client.on("message", message => {
-    if (message.content.startsWith("wbc")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-  m.send(`${argresult}\n ${m}`);
-  })
-  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
-  message.delete();
-  };
-});
-
-
-//bc online
-
-
-
-
-  client.on("message", message => {
-  
-              if (message.content.startsWith(prefix + "obc")) {
-                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-    let args = message.content.split(" ").slice(1);
-    var argresult = args.join(' '); 
-    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
-   m.send(`${argresult}\n ${m}`);
-  })
-   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
-   message.delete(); 
-  };     
-});
-
 
 
 client.login(process.env.TOKEN);
