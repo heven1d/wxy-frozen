64af5da6d12d3fddeb273b4c5f7abfe2d99b325b
heven1d <44444524+heven1d@users.noreply.github.com>
2018-12-09 16:04:37 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require("Discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -9,6 +9,52 @@ client.user.setActivity("You.",{type: 'LISTENING'});
   console.log('╔[═══════════════════════════════]╗')
   console.log(`[Start] ${new Date()}`);
 });
+
+
+
+client.on('message', message => {
+     if (message.content === "باي") {
+message.channel.sendMessage("" + " :rose:باي");
+    }
+}); 
+
+
+client.on('message', message => {
+     if (message.content === "spam?") {
+message.channel.sendMessage("يلا");
+    }
+}); 
+
+
+client.on('message', message => {
+     if (message.content === "برب") {
+message.channel.sendMessage("" + " :rose:تيت");
+    }
+}); 
+
+
+client.on('message', message => {
+     if (message.content === "هاي") {
+message.channel.sendMessage("" + "هايي");
+    }
+}); 
+
+
+
+client.on('message', message => {
+     if (message.content === "هلوو") {
+message.channel.sendEmbed("tt" + ":heart: هلوو");
+    }
+}); 
+
+
+client.on('message', message => {
+     if (message.content === "السلام " + "عليكم") {
+message.channel.sendMessage("" + "وعليكم السلام");
+    }
+});     
+
+
 
 
 client.login(process.env.BOT_TOKEN);
