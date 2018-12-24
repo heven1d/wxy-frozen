9c21fba29a9d1b5f594cb4c5e8f4fb1063d53b74
heven1d <44444524+heven1d@users.noreply.github.com>
2018-12-24 22:22:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,50 @@ member.addRole(member.guild.roles.find('name', '› Ads.'));
 
 
 
+ client.on('message', message => {
+    if (message.content.split(' ')[0] == '!bc')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( ` ${member} ` + "**" + "  ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   client.on("message", message => {
+       var prefix = "!";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "bc")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+
+
+
+
+
+
 client.on('message', message => {
      if (message.content === "باي") {
 message.channel.sendMessage("" + " :rose:باي");
